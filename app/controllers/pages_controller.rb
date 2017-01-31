84b87d5a5511d25cc1172c0a2eeb90b2cb388e9d
Graham Moss <graham.moss@hotmail.com>
2017-01-31 14:50:25 -0500

    added working flash notice

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,8 +14,8 @@ class PagesController < ApplicationController
   end
 
   def contest
-
-    @header = "this is the contest page"
+    flash[:notice] = "Sorry, the contest has ended"
+    redirect_to "/welcome"
 
   end
 
