d70e6f31ec3efdc2c982546b800d70b3a086dfaa
Graham Moss <graham.moss@hotmail.com>
2017-01-31 14:06:06 -0500

    gave values to @header in all instance methods

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,14 +2,19 @@ class PagesController < ApplicationController
 
   def welcome
 
+    @header = "neat"
 
   end
 
   def about
 
+    @header = "cool"
+
   end
 
   def contest
+
+    @header = "this is the contest page"
 
   end
 
